513c8f45be1b360e0d94b6f55350defbdcff7995
devalexedmondson <alexedmondson20@gmail.com>
2017-03-15 11:43:50 -0500

    Added user roles, changed bootstrap layout

---
+++ TripSplit/TripSplit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TripSplit.MvcApplication" Language="C#" %>
---
+++ TripSplit/TripSplit/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using TripSplit.Models;
+
+[assembly: OwinStartupAttribute(typeof(TripSplit.Startup))]
+namespace TripSplit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+        public void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Admin super that maintains website
+
+                var user = new ApplicationUser();
+                user.UserName = "Admin";
+                user.Email = "alexedmondson20@gmail.com";
+
+                string userPass = "Password1234";
+
+                var chkUser = UserManager.Create(user, userPass);
+
+                //Adds default User to Role Admin
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+            //Create A User
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
